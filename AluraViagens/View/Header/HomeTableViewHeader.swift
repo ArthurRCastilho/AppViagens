5bc62ba7e708540fcd83518c465d7ae88e0a34e7
ArthurLVidal <arthurodri2017@gmail.com>
2025-01-31 13:10:37 -0300

    inclusao de imagem

--- AluraViagens/View/Header/HomeTableViewHeader.swift
+++ AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -14,9 +14,16 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var bannerView: UIView!
     
     func configurarView() {
         headerView.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 199/255, alpha: 1)
         bannerImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 500
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
 }
