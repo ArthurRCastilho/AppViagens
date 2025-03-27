423bf49d0b7a5adcdb762eab9991f7519948cb72
ArthurLVidal <arthurodri2017@gmail.com>
2025-03-27 15:01:20 -0300

    Add ViagemTableViewCell

---
+++ AluraViagens/View/Cells/ViagemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViagemTableViewCell.swift
+//  AluraViagens
+//
+//  Created by Arthur Rodrigues on 27/03/25.
+//
+
+import UIKit
+
+class ViagemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
